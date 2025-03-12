08437499382203a652d5ec0476a01e034681e0a7
Matthias Wiesmann <matthiaswiesmann@device-75.home>
2025-03-12 17:32:45 +0100

    ka

--- project-scrapeflow/app/workflow/_components/nodes/NodeParamField.tsx
+++ project-scrapeflow/app/workflow/_components/nodes/NodeParamField.tsx
@@ -6,28 +6,49 @@ import React, { useCallback } from "react";
 import StringParam from "./param/StringParam";
 import { useReactFlow } from "@xyflow/react";
 import { ScrapeFlowNode } from "@/types/scrapeFlowNode";
+import BrowserInstanceParam from "./param/BrowserInstanceParam";
 
 function NodeParamField({
   param,
   nodeId,
+  disabled,
 }: {
   param: TaskParam;
   nodeId: string;
+  disabled?: boolean;
 }) {
   const { updateNodeData, getNode } = useReactFlow();
   const node = getNode(nodeId) as ScrapeFlowNode;
   const value = node?.data.inputs?.[param.name];
-  const updateNodeParamValue = useCallback((newValue: string) => {
-    updateNodeData(nodeId, {
-      inputs: {
-        ...node?.data.inputs,
-        [param.name]: newValue,
-      },
-    });
-  }, [updateNodeData, nodeId, param.name, node?.data.inputs]);
+  const updateNodeParamValue = useCallback(
+    (newValue: string) => {
+      updateNodeData(nodeId, {
+        inputs: {
+          ...node?.data.inputs,
+          [param.name]: newValue,
+        },
+      });
+    },
+    [updateNodeData, nodeId, param.name, node?.data.inputs]
+  );
   switch (param.type) {
     case TaskParameterType.STRING:
-      return <StringParam param={param} value={value} updateNodeParamValue={updateNodeParamValue} />;
+      return (
+        <StringParam
+          param={param}
+          value={value}
+          updateNodeParamValue={updateNodeParamValue}
+          disabled={disabled}
+        />
+      );
+    case TaskParameterType.BROWSER_INSTANCE:
+      return (
+        <BrowserInstanceParam
+          param={param}
+          value={""}
+          updateNodeParamValue={updateNodeParamValue}
+        />
+      );
     default:
       return (
         <div className="w-full">
