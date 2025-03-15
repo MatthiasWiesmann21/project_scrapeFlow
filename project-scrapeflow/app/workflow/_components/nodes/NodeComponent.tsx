ef8156a9210ad5b9c1c1b3cb4a5c8c35922e1508
Matthias Wiesmann <matthiaswiesmann@device-75.home>
2025-03-15 15:41:08 +0100

    adding validation

--- project-scrapeflow/app/workflow/_components/nodes/NodeComponent.tsx
+++ project-scrapeflow/app/workflow/_components/nodes/NodeComponent.tsx
@@ -6,25 +6,29 @@ import { scrapeFlowNodeData } from "@/types/scrapeFlowNode";
 import { TaskRegistry } from "@/lib/workflow/task/registry";
 import { NodeInput, NodeInputs } from "./NodeInputs";
 import { NodeOutput, NodeOutputs } from "./NodeOutputs";
+import { Badge } from "@/components/ui/badge";
+
+const DEV_NODE = process.env.NEXT_PUBLIC_DEV_MODE === "true";
 
 const NodeComponent = memo((props: NodeProps) => {
-    const nodeData = props.data as scrapeFlowNodeData;
-    const task = TaskRegistry[nodeData.type];
-    return (
-        <NodeCard nodeId={props.id} isSelected={!!props.selected}>
-           <NodeHeader taskType={nodeData.type} nodeId={props.id} />
-           <NodeInputs>
-            {task.inputs.map(input => (
-                <NodeInput key={input.name} input={input} nodeId={props.id} />
-            ))}
-           </NodeInputs>
-           <NodeOutputs>
-            {task.outputs.map(output => (
-                <NodeOutput key={output.name} output={output}/>
-            ))}
-           </NodeOutputs>
-        </NodeCard>
-    );
+  const nodeData = props.data as scrapeFlowNodeData;
+  const task = TaskRegistry[nodeData.type];
+  return (
+    <NodeCard nodeId={props.id} isSelected={!!props.selected}>
+      {DEV_NODE && <Badge>DEV:{props.id}</Badge>}
+      <NodeHeader taskType={nodeData.type} nodeId={props.id} />
+      <NodeInputs>
+        {task.inputs.map((input) => (
+          <NodeInput key={input.name} input={input} nodeId={props.id} />
+        ))}
+      </NodeInputs>
+      <NodeOutputs>
+        {task.outputs.map((output) => (
+          <NodeOutput key={output.name} output={output} />
+        ))}
+      </NodeOutputs>
+    </NodeCard>
+  );
 });
 
 export default NodeComponent;
