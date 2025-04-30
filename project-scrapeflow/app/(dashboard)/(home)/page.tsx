686b29f90c757988b14ad6a9e96d3bea198ff81c
Matthias Wiesmann <matthiaswiesmann@MacBook-Pro-von-Matthias.local>
2025-04-30 18:10:25 +0200

    starting with hoempage

--- project-scrapeflow/app/(dashboard)/(home)/page.tsx
+++ project-scrapeflow/app/(dashboard)/(home)/page.tsx
@@ -1,9 +1,46 @@
-import React from 'react'
+import { GetPeriods } from "@/actions/analytics/getPeriods";
+import React, { Suspense } from "react";
+import PeriodSelector from "./_components/PeriodSelector";
+import { Period } from "@/types/analytics";
+import { waitFor } from "@/lib/helper/waitFor";
+import { Skeleton } from "@/components/ui/skeleton";
+import { GetStatsCardValues } from "@/actions/analytics/getStatsCardValues";
 
-function HomePage() {
+function HomePage({
+  searchParams,
+}: {
+  searchParams: { month?: string; year?: string };
+}) {
+  const currentDate = new Date();
+  const { month, year } = searchParams;
+  const period: Period = {
+    month: month ? parseInt(month) : currentDate.getMonth(),
+    year: year ? parseInt(year) : currentDate.getFullYear(),
+  };
   return (
-    <div>Home</div>
-  )
+    <div className="flex flex-1 flex-col h-full">
+      <div className="flex justify-between">
+        <h1 className="text-3xl font-bold">Home</h1>
+        <Suspense fallback={<Skeleton className="w-[180px] h-40" />}>
+          <PeriodSelectorWrapper selectedPeriod={period} />
+        </Suspense>
+      </div>
+      <StatsCards selectedPeriod={period} />
+    </div>
+  );
 }
 
-export default HomePage
+async function PeriodSelectorWrapper({
+  selectedPeriod,
+}: {
+  selectedPeriod: Period;
+}) {
+  const periods = await GetPeriods();
+  return <PeriodSelector periods={periods} selectedPeriod={selectedPeriod} />;
+}
+export default HomePage;
+
+async function StatsCards({ selectedPeriod }: { selectedPeriod: Period }) {
+  const data = await GetStatsCardValues(selectedPeriod)
+  return <pre>{JSON.stringify(data, null, 4)}</pre>
+}
--- project-scrapeflow/lib/helper/dates.ts
+++ project-scrapeflow/lib/helper/dates.ts
@@ -1,4 +1,5 @@
-import { intervalToDuration } from "date-fns";
+import { Period } from "@/types/analytics";
+import { endOfMonth, intervalToDuration, startOfMonth } from "date-fns";
 
 export function DatesToDurationString(
   end: Date | null | undefined,
@@ -20,3 +21,12 @@ export function DatesToDurationString(
 
   return `${duration.minutes || 0}m ${duration.seconds || 0}s`;
 }
+
+export function PeriodToDateRange(period: Period) {
+    const startDate = startOfMonth(period.year, period.month);
+    const endDate = endOfMonth(period.year, period.month);
+    return {
+      startDate,
+      endDate,
+    }
+  }
